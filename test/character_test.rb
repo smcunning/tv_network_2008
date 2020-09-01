1bbe546777494cc1283e66da4c899117c373250e
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-01 09:08:15 -0600

    Add test for attributes

--- test/character_test.rb
+++ test/character_test.rb
@@ -9,4 +9,12 @@ class CharacterTest < Minitest::Test
 
     assert_instance_of Character, kitt
   end
+
+  def test_it_has_attributes
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+    assert_equals "KITT", kitt.name
+    assert_equals "William Danniels", kitt.actor
+    assert_equals 1_000_000, kitt.salary
+  end
 end
