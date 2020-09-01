28b80e00c37550909dd06e24c7dac6c555c4fa4f
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-01 09:06:32 -0600

    Add initialize and attributes

--- lib/character.rb
+++ lib/character.rb
@@ -1,4 +1,11 @@
 class Character
 
+  def initialize(details)
+    @details = details
+    @name = @details[:name]
+    @actor = @details[:actor]
+    @salary = @details[:salary]
+  end
 
-end 
+
+end
