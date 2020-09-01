765941c72b0bd7775c753858ede72c5bbb036e26
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-01 10:17:46 -0600

    Add method for main_characters

--- lib/network.rb
+++ lib/network.rb
@@ -8,4 +8,14 @@ attr_reader :name, :shows
   def add_show(show)
     @shows << show
   end
+
+  def main_characters
+  all_characters = @shows.map do |show|
+      show.characters
+    end
+  all_characters = all_characters.flatten
+  main_characters = all_characters.find_all do |character|
+      character.salary > 500_000 && character.name == character.name.upcase
+    end
+  end
 end
