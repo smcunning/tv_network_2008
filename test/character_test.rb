6f4033073477ee25ea4133654caa1abdbea182c4
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-01 09:04:59 -0600

    Add test for instance of Character

--- test/character_test.rb
+++ test/character_test.rb
@@ -4,4 +4,9 @@ require "./lib/character"
 
 class CharacterTest < Minitest::Test
 
+  def test_it_exists
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+    assert_instance_of Character, kitt
+  end
 end
