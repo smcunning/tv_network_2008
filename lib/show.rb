138e449fa514a506c853b0bf36f8646251e1aa3f
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-01 09:25:57 -0600

    Add method for highest_paid_actor

--- lib/show.rb
+++ lib/show.rb
@@ -9,6 +9,13 @@ class Show
   def total_salary
     @characters.sum do |character|
       character.salary
-    end 
+    end
+  end
+
+  def highest_paid_actor
+    highest_paid = @characters.max_by do |character|
+      character.salary
+    end
+    highest_paid.details[:actor]
   end
 end
